0f7c99473662bf1c43b0c3b0190c020e4d9fe6e8
pkabelka <pkabelka@email.cz>
2023-04-26 22:24:00 +0200

    added example reading from STDIN

---
+++ example_stdin.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define SSTR_IMPLEMENTATION
+#include "sstr.h"
+
+int main()
+{
+    sstr s = sstr_new("");
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        sstr_add_char(&s, c);
+    }
+    printf("%ld, %ld, %s\n", s.length, s.capacity, s.cstr);
+
+    return 0;
+}
