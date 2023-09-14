112da2fd91a5bd4e940a465922e7f382000e32de
pkabelka <pkabelka@email.cz>
2023-09-14 16:52:44 +0200

    added sfft_recursive.h (recursive implementation of FFT)

---
+++ examples/sfft_recursive_example.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <complex.h>
+
+#define SFFT_RECURSIVE_IMPLEMENTATION
+#include "../sfft_recursive.h"
+
+int main()
+{
+    size_t const n = 8;
+
+    double complex in[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    double complex *out = (double complex *) malloc(sizeof(double complex) * n);
+
+    /* forward transform */
+    sfft_recursive_fft(in, out, n);
+
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("%g %gj    ", creal(out[i]), cimag(out[i]));
+    }
+    printf("\n");
+    /* 36 0j    -4 9.65685j    -4 4j    -4 1.65685j    -4 0j    -4 -1.65685j    -4 -4j    -4 -9.65685j    */
+
+    /* inverse transform */
+    sfft_recursive_ifft(out, in, n);
+
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("%g %gj    ", creal(in[i]), cimag(in[i]));
+    }
+    printf("\n");
+    /* 1 0j    2 -2.71835e-16j    3 4.44089e-16j    4 3.82857e-16j    5 0j    6 -4.979e-17j    7 -4.44089e-16j    8 -6.12323e-17j    */
+
+    free(out);
+
+    return 0;
+}
---
+++ sfft_recursive.h
@@ -0,0 +1,151 @@
+/** This is free and unencumbered software released into the public domain.
+  * 
+  * Anyone is free to copy, modify, publish, use, compile, sell, or
+  * distribute this software, either in source code form or as a compiled
+  * binary, for any purpose, commercial or non-commercial, and by any
+  * means.
+  * 
+  * In jurisdictions that recognize copyright laws, the author or authors
+  * of this software dedicate any and all copyright interest in the
+  * software to the public domain. We make this dedication for the benefit
+  * of the public at large and to the detriment of our heirs and
+  * successors. We intend this dedication to be an overt act of
+  * relinquishment in perpetuity of all present and future rights to this
+  * software under copyright law.
+  * 
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  * OTHER DEALINGS IN THE SOFTWARE.
+  * 
+  * For more information, please refer to <http://unlicense.org/>
+  */
+
+#ifndef INCLUDE_SFFT_RECURSIVE_H
+#define INCLUDE_SFFT_RECURSIVE_H
+
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+#include <complex>
+typedef std::complex<double> sfft_recursive_complexdouble;
+#else
+typedef double complex sfft_recursive_complexdouble;
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Recursively computes the Fast Fourier transform of complex numbers in array
+ * `in` into the array `out`. Both arrays must be allocated before calling this
+ * function.
+ *
+ * @param in Input array of complex numbers.
+ * @param out Output array of complex numbers. `out` must have a capacity for at
+ * least `n` numbers.
+ * @param n Number of elements inside array `in`.
+ * @return True if the function succeedes, false otherwise.
+ */
+bool sfft_recursive_fft(sfft_recursive_complexdouble * const in,
+                        sfft_recursive_complexdouble * const out,
+                        size_t const n);
+
+/**
+ * Recursively computes the Inverse Fast Fourier transform of complex numbers in
+ * array `in` into the array `out`. Both arrays must be allocated before calling
+ * this function.
+ *
+ * @param in Input array of complex numbers.
+ * @param out Output array of complex numbers. `out` must have a capacity for at
+ * least `n` numbers.
+ * @param n Number of elements inside array `in`.
+ * @return True if the function succeedes, false otherwise.
+ */
+bool sfft_recursive_ifft(sfft_recursive_complexdouble * const in,
+                         sfft_recursive_complexdouble * const out,
+                         size_t const n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*INCLUDE_SFFT_RECURSIVE_H*/
+
+#ifdef SFFT_RECURSIVE_IMPLEMENTATION
+
+#ifndef _SFFT_RECURSIVE_PI
+#define _SFFT_RECURSIVE_PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117068
+#endif
+
+void _sfft_recursive(sfft_recursive_complexdouble * const in,
+                     sfft_recursive_complexdouble * const out,
+                     size_t const n,
+                     size_t const offset,
+                     bool const forward)
+{
+    if (n <= 1)
+    {
+        out[0] = in[0];
+        return;
+    }
+
+    const sfft_recursive_complexdouble omega_n = cexp((forward ? -2 : 2) * I * _SFFT_RECURSIVE_PI / n);
+    sfft_recursive_complexdouble omega = 1;
+
+    /* even */
+    _sfft_recursive(in, out, n/2, offset*2, forward);
+    /* odd */
+    _sfft_recursive(in+offset, out+n/2, n/2, offset*2, forward);
+
+    for (size_t k = 0; k < n/2; k++)
+    {
+        const sfft_recursive_complexdouble even = out[k];
+        const sfft_recursive_complexdouble odd = out[k + n/2];
+        out[k] = even + omega * odd;
+        out[k + n/2] = even - omega * odd;
+        omega = omega * omega_n;
+    }
+}
+
+bool sfft_recursive_fft(sfft_recursive_complexdouble * const in,
+                        sfft_recursive_complexdouble * const out,
+                        size_t const n)
+{
+    if (n == 0 || (n & (n - 1)) != 0 || in == NULL || out == NULL)
+    {
+        return false;
+    }
+
+    _sfft_recursive(in, out, n, 1, true);
+
+    return true;
+}
+
+bool sfft_recursive_ifft(sfft_recursive_complexdouble * const in,
+                         sfft_recursive_complexdouble * const out,
+                         size_t const n)
+{
+    if (n == 0 || (n & (n - 1)) != 0 || in == NULL || out == NULL)
+    {
+        return false;
+    }
+
+    _sfft_recursive(in, out, n, 1, false);
+
+    for (size_t i = 0; i < n; i++)
+    {
+        out[i] /= n;
+    }
+
+    return true;
+}
+
+#endif /*SFFT_RECURSIVE_IMPLEMENTATION*/
